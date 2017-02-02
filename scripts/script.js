f911048d8fa2d8fc08ccf545ff29672daaa70347
mrantua <ivan.kramnuk@gmail.com>
2017-02-02 13:18:51 +0200

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,23 @@
-/**************** Paralax 1 (With JQuery) ************/
+var s = Snap("#svg");
 
-$(".container-1").mousemove(function(e){
-	// take now position of cursor
-	var position = e.pageX;
-	// and count animation coefficient
-	position /= 100;
-	// change value of pripwrty for animation
-	$(".container-1__layer-2").css("transform", "translateX(" + position + "px)"); 
-	$(".container-1__layer-1").css("transform", "translateX( -" + position + "px)");
-	$(".container-1__layer-3").css("transform", "translateX( -" + position + "px)");
-})
+var grayBuild1 = s.select("#rect7931");
+grayBuild1.attr({y:1000});
+var grayBuild2 = s.select("#rect7781");
+grayBuild2.attr({y:1000});
+var grayBuild3 = s.select("#rect7609");
+grayBuild3.attr({y:1000});
+var grayBuild4 = s.select("#rect7605");
+grayBuild4.attr({y:1000});
+var grayBuild5 = s.select("#rect7699");
+grayBuild5.attr({y:1000});
+var grayBuild6 = s.select("#rect7679");
+grayBuild6.attr({y:1000});
 
-/**************** Some animation with JQuery ************/
-
-// Take mark
-var markPosition = $(".text-mark").offset();
-var animPosition = Math.floor(markPosition.top);
-$(document).scroll(function(){
-	// Take verticle center of the window
-	var windowPosition = $(document).scrollTop() + ($(window).height() / 2);
-	// Whatch when user go closer to the our block
-	if(windowPosition > animPosition){
-		// Show our articles with some animation
-		$(".container-4__some-block").animate({
-			"top"    : "0px",
-			"opacity" : "1"
-		}, 1000);
-	}
-})
+(function firstBuild(){
+	grayBuild1.animate({y : -250}, 2000,mina.easeinout);
+	grayBuild2.animate({y : -525}, 1000,mina.easeinout);
+	grayBuild3.animate({y : -250}, 2000,mina.easeinout);
+	grayBuild4.animate({y : -370}, 2000,mina.easeinout);
+	grayBuild5.animate({y : -420}, 2000,mina.easeinout);
+	grayBuild6.animate({y : -970}, 2000,mina.easeinout);
+})();
