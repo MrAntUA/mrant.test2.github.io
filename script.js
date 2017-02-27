9c140ea521e1394f48f58a09b90bc470dc7b2b81
mrantua <ivan.kramnuk@gmail.com>
2017-02-27 11:15:57 +0200

    commit

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 (function main(){
-
+	var i = 0;
+	var j = 10;
 	function createElements(){
-		for(var i = 0; i < 50; i++){
+		for(i ; i < j; i++){
 			$("body").append("<div class=\"element\"></div>");
 			setColor($(".element")[i]);
 			moveElement($(".element")[i]);
 		}
+		j += 1;
 	}
 
 	function setColor(element){
@@ -19,9 +21,10 @@
 		var randSpeed = Math.round(3 - 0.5 + Math.random() * (10 - (3) + 1));
 		var elTop = parseInt($(element).css("top"));
 		var elLeft = parseInt($(element).css("left"));
-		$(element).css({"top": elTop + randPosX + "px", "left": elLeft + randPosY + "px", "opacity":"0", "transition": randSpeed + "s", "transform":"scale(10)"});
+		$(element).css({"top": elTop + randPosX + "px", "left": elLeft + randPosY + "px", "opacity":"0", "transition": randSpeed + "s"});
 	}
 
-	createElements();
+	setInterval(createElements, 100);
+	//createElements();
 
 })();
