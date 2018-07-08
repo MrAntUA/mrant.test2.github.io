4e68b174bb4996dfa696ab049a2267b02b461a42
MrAntUA <i.kramnyuk@gmail.com>
2018-07-08 16:33:50 +0300

    responsive 80%

--- js/script.js
+++ js/script.js
@@ -5,6 +5,16 @@ $(".scroll-marker").click(function() {
     }, 1000);
 });
 
+$(".mobile-btn").on("click", function(){
+
+	$(".main-header .main-menu .menu-container").addClass("menu-open");
+});
+
+$(".close-menu").on("click", function(){
+
+	$(".main-header .main-menu .menu-container").removeClass("menu-open");
+});
+
 $(document).ready(function(){
   $('.slider-container').slick({
     prevArrow: $(".slider-prev"),
