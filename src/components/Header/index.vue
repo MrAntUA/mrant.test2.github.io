ad49e454bcdb15be0d43b4eecd3abcb6e64f40a8
Ivan Kramnyuk <i.kramnyuk@gmail.com>
2020-03-11 04:27:40 +0200

    changes

---
+++ src/components/Header/index.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="gradiented-wrapp">
+        <div class="custom-container">
+            <header class="main-header container">
+                <nav class="main-menu">
+                    <a href="#" class="logo" @click="goTo('/')">
+                        <img src="@/assets/img/logo.png" alt="logo">
+                    </a>
+
+                    <a href="#">Monitoring</a>
+                    <a href="#">Eligibility</a>
+                    <a href="#">Offer building</a>
+                    <a href="#">Partnership</a>
+                </nav>
+
+                <nav>
+                    <a href="#" @click="goTo('/about')">About</a>
+                    <a href="#" @click="goTo('/contact')">Contact</a>
+                </nav>
+            </header>
+        </div>
+    </div>
+</template>
+
+<script>
+    import './style.scss';
+    
+    export default {
+        name: "Header",
+
+        components: {},
+        
+        data() {
+            return {};
+        },
+
+        methods: {
+            goTo(route){
+                this.$router.push({ path: route })
+            }
+        }
+    };
+</script>
