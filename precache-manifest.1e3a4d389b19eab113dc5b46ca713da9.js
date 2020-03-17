61d6eaaa6ebb6a8b13e405e83d9c99827dfc71e8
Ivan Kramnyuk <i.kramnyuk@gmail.com>
2020-03-18 00:24:35 +0200

    Updates

---
+++ precache-manifest.1e3a4d389b19eab113dc5b46ca713da9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44855dd481ff2b3e90e358138ca89ee7",
+    "url": "/mrant.test2.github.io/index.html"
+  },
+  {
+    "revision": "ec5ee15c725168ffa3f9",
+    "url": "/mrant.test2.github.io/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "22b8a43ccc556be7afa9",
+    "url": "/mrant.test2.github.io/static/css/main.9f9d41d8.chunk.css"
+  },
+  {
+    "revision": "ec5ee15c725168ffa3f9",
+    "url": "/mrant.test2.github.io/static/js/2.806a6507.chunk.js"
+  },
+  {
+    "revision": "cb74030c757e88ed41d1616d3443d3e2",
+    "url": "/mrant.test2.github.io/static/js/2.806a6507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b8a43ccc556be7afa9",
+    "url": "/mrant.test2.github.io/static/js/main.9841b48b.chunk.js"
+  },
+  {
+    "revision": "47aa504878212a4eefc0",
+    "url": "/mrant.test2.github.io/static/js/runtime-main.47fa4b21.js"
+  },
+  {
+    "revision": "1a26c22e3901cf254c6434290672aa15",
+    "url": "/mrant.test2.github.io/static/media/accounts-active.1a26c22e.svg"
+  },
+  {
+    "revision": "0b9339193e1975bafb3828d331231ac1",
+    "url": "/mrant.test2.github.io/static/media/calendar.0b933919.svg"
+  },
+  {
+    "revision": "2b36db2f100833db6fffec1f2051efe7",
+    "url": "/mrant.test2.github.io/static/media/chip.2b36db2f.svg"
+  },
+  {
+    "revision": "55a6fa733138d130e07798c727de72ab",
+    "url": "/mrant.test2.github.io/static/media/clients-active.55a6fa73.svg"
+  },
+  {
+    "revision": "f045f0f6acdf85714c36b5809a005713",
+    "url": "/mrant.test2.github.io/static/media/device-active.f045f0f6.svg"
+  },
+  {
+    "revision": "e840bd6af15cee2d92a6baca0740d691",
+    "url": "/mrant.test2.github.io/static/media/down-arrow.e840bd6a.svg"
+  },
+  {
+    "revision": "9501fe4d9ded800b9ca0d4580d940268",
+    "url": "/mrant.test2.github.io/static/media/gear.9501fe4d.svg"
+  },
+  {
+    "revision": "38e28fdcb4475a22684948c154eac19a",
+    "url": "/mrant.test2.github.io/static/media/logo.38e28fdc.svg"
+  },
+  {
+    "revision": "b6890bc9c95de59e1da270a7c39b31b9",
+    "url": "/mrant.test2.github.io/static/media/logout.b6890bc9.svg"
+  },
+  {
+    "revision": "51c44790b31f99759ffa25c30cca8687",
+    "url": "/mrant.test2.github.io/static/media/mail.51c44790.svg"
+  },
+  {
+    "revision": "c749c64966ea5e6251869bcbc69b9674",
+    "url": "/mrant.test2.github.io/static/media/search.c749c649.svg"
+  },
+  {
+    "revision": "f5936394f01b64c7093e5fa346a72ad8",
+    "url": "/mrant.test2.github.io/static/media/team.f5936394.svg"
+  }
+]);
