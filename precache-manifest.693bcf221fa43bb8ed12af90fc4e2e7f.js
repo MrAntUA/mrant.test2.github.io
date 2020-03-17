305c538349a0ed4894bac32b8b363896579c1b32
Ivan Kramnyuk <i.kramnyuk@gmail.com>
2020-03-18 00:12:18 +0200

    changes

---
+++ precache-manifest.693bcf221fa43bb8ed12af90fc4e2e7f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e80d798b638ed42f5e4aab636a2866",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec5ee15c725168ffa3f9",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "f27ed60be5cdc27a7b50",
+    "url": "/static/css/main.9f9d41d8.chunk.css"
+  },
+  {
+    "revision": "ec5ee15c725168ffa3f9",
+    "url": "/static/js/2.806a6507.chunk.js"
+  },
+  {
+    "revision": "cb74030c757e88ed41d1616d3443d3e2",
+    "url": "/static/js/2.806a6507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f27ed60be5cdc27a7b50",
+    "url": "/static/js/main.4b78139e.chunk.js"
+  },
+  {
+    "revision": "8e33a506ae3410f5e968",
+    "url": "/static/js/runtime-main.c50dacf0.js"
+  },
+  {
+    "revision": "1a26c22e3901cf254c6434290672aa15",
+    "url": "/static/media/accounts-active.1a26c22e.svg"
+  },
+  {
+    "revision": "0b9339193e1975bafb3828d331231ac1",
+    "url": "/static/media/calendar.0b933919.svg"
+  },
+  {
+    "revision": "2b36db2f100833db6fffec1f2051efe7",
+    "url": "/static/media/chip.2b36db2f.svg"
+  },
+  {
+    "revision": "55a6fa733138d130e07798c727de72ab",
+    "url": "/static/media/clients-active.55a6fa73.svg"
+  },
+  {
+    "revision": "f045f0f6acdf85714c36b5809a005713",
+    "url": "/static/media/device-active.f045f0f6.svg"
+  },
+  {
+    "revision": "e840bd6af15cee2d92a6baca0740d691",
+    "url": "/static/media/down-arrow.e840bd6a.svg"
+  },
+  {
+    "revision": "9501fe4d9ded800b9ca0d4580d940268",
+    "url": "/static/media/gear.9501fe4d.svg"
+  },
+  {
+    "revision": "38e28fdcb4475a22684948c154eac19a",
+    "url": "/static/media/logo.38e28fdc.svg"
+  },
+  {
+    "revision": "b6890bc9c95de59e1da270a7c39b31b9",
+    "url": "/static/media/logout.b6890bc9.svg"
+  },
+  {
+    "revision": "51c44790b31f99759ffa25c30cca8687",
+    "url": "/static/media/mail.51c44790.svg"
+  },
+  {
+    "revision": "c749c64966ea5e6251869bcbc69b9674",
+    "url": "/static/media/search.c749c649.svg"
+  },
+  {
+    "revision": "f5936394f01b64c7093e5fa346a72ad8",
+    "url": "/static/media/team.f5936394.svg"
+  }
+]);
